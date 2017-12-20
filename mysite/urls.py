8cec427502e0c39656ad6eeff03253c5ba0b963f
Chris Kavanagh <ckava3@gmail.com>
2017-12-20 00:06:31 -0500

    2nd Commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -26,7 +26,6 @@ urlpatterns = [
 	path('', home_view, name='home_view'),
 	path('myuser/', include('myuser.urls')),
     path('contact/', include('contact.urls')),
-    #path('sitemap.xml', sitemap, {'sitemaps': sitemaps},name='django.contrib.sitemaps.views.sitemap'),
     path('admin/', admin.site.urls),
     
 ]
